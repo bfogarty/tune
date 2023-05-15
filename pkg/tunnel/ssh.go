591305fbe9709a67ab3f13bf4592866e2e7cc5c8
Brian Fogarty <brian@fogarty.email>
2023-05-14 21:50:20 -0500

    ed25519 support

--- pkg/tunnel/ssh.go
+++ pkg/tunnel/ssh.go
@@ -1,32 +1,36 @@
 package tunnel
 
 import (
+	"crypto/ed25519"
 	"crypto/rand"
-	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"golang.org/x/crypto/ssh"
 )
 
 func generateKeyPair() ([]byte, []byte, error) {
-	// generate a new 2048-bit RSA key
-	rsaKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	// generate a new ed25519 keypair
+	publicKey, privateKey, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	// encode the private key in PEM format
-	privateKeyBytes := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(rsaKey),
-	})
-
-	// encode the public key for inclusion in authorized_keys
-	publicKey, err := ssh.NewPublicKey(&rsaKey.PublicKey)
+	privKeyPKCS8, err := x509.MarshalPKCS8PrivateKey(privateKey)
 	if err != nil {
 		return nil, nil, err
 	}
-	publicKeyBytes := ssh.MarshalAuthorizedKey(publicKey)
+	privateKeyBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: privKeyPKCS8,
+	})
+
+	// encode the public key for inclusion in authorized_keys
+	sshPublicKey, err := ssh.NewPublicKey(publicKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	publicKeyBytes := ssh.MarshalAuthorizedKey(sshPublicKey)
 
 	return privateKeyBytes, publicKeyBytes, nil
 }
